511acadf05ec014943e123dc1a9b8f6ce1dbc67f
Robert Labrie <robert.labrie@gmail.com>
2013-08-02 03:36:25 -0700

    populate wrapper class props from WMI native class\

--- SkeletonNative.php
+++ SkeletonNative.php
@@ -11,6 +11,7 @@ class __classname
 	public function __construct($obj)
 	{
 		$this->obj = $obj;
+		$this->populateproperties();
 	}
 	public function __get($key)
 	{
@@ -24,6 +25,16 @@ class __classname
 			
 			//try to read the object property back out to store in our local copy
 			$this->$key = $this->obj->$key;
+		}
+	}
+	/**
+	* populate the properties of the wrapper class with the properties of the native WMI class
+	**/
+	public function populateproperties()
+	{
+		foreach ($this->properties as $p)
+		{
+			$this->$p = $this->obj->$p;
 		}
 	}
 }
